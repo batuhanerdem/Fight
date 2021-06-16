2efbcd2e79aa12ef008a6ea96a50a93912ad82e4
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2021-06-16 21:32:51 +0300

    Update Skills.py

--- Skills.py
+++ Skills.py
@@ -3,10 +3,9 @@ import random
 
 
 def passives(char):
-    # char.rage += random.randint(10, 25)
     char.rage += 5
 
-    if char == Character.kevin:
+    if char == Character.kevin:  # Kevin's passive
         critChance = random.randint(1, 5)  # %20 crit change
         if critChance == 5:
             char.crit = True
@@ -14,14 +13,14 @@ def passives(char):
             char.crit = False  # when character crits the round before this, if critChance is not 5 which means
             # our char do not crit, this else block changes char.crit back to false.
 
-    if char == Character.fara:
+    if char == Character.fara:  # Fara's passive
         dodgeChance = random.randint(1, 5)
         if dodgeChance == 5:
             char.dodge = True
         else:
             char.dodge = False
 
-    if char == Character.ken:
+    if char == Character.ken:  # Ken's passive
         critChance = random.randint(1, 5)
         dodgeChance = random.randint(1, 5)
         if critChance == 5:
@@ -34,19 +33,19 @@ def passives(char):
             char.dodge = False
 
     if char.passive:
-        if char == Character.kevin:
+        if char == Character.kevin:  # Ken's active passive
             if char.health <= 150:
                 char.attack *= 2
-                print(f'Kevin passive actived; %100 crit chance on')
+                print(f'Kevin passive actived; new attack is {char.attack}')
                 char.passive = False
 
-        if char == Character.fara:
+        if char == Character.fara:  # Fara's active passive
             if char.health <= 250:
                 char.deffence *= 1.5
                 print(f'Fara passive actived; new defence {char.deffence}')
                 char.passive = False
 
-        if char == Character.ken:
+        if char == Character.ken:  # Ken's active passive
             if char.health <= 200:
                 char.attack += int(char.attack / 2)
                 char.deffence += int(char.deffence / 2)
@@ -57,18 +56,18 @@ def passives(char):
 def skill(char):
     choice = input(f"Please press 1-2-3 for {char.name}'s skills: ")  # Choising skill
 
-    if char == Character.kevin and choice == "1":  # Kevins 1. skill gives 5 rage
+    if char == Character.kevin and choice == "1":  # Kevin's 1. skill gives 5 rage
         char.rage += 5
-    if char == Character.kevin and choice == "2":  # Kevins 2. skill takes 10 rage
+    if char == Character.kevin and choice == "2":  # Kevin's 2. skill costs 10 rage makes attack 2x
         if char.rage >= 10:
             char.attack *= 2
-            char.skill2 = True
             char.rage -= 10
+            char.skill2 = True
         else:
             print("You dont have enough rage.")
             skill(char)
             return
-    if char == Character.kevin and choice == "3":  # Kevins ult.
+    if char == Character.kevin and choice == "3":  # Kevin's ult makes attack 4x
         if char.rage >= 50:
             char.attack *= 4
             char.rage -= 50
@@ -78,18 +77,18 @@ def skill(char):
             skill(char)
             return
 
-    if char == Character.fara and choice == "1":
+    if char == Character.fara and choice == "1":  # Fara's 1. skill gives 5 rage
         char.rage += 5
-    if char == Character.fara and choice == "2":
+    if char == Character.fara and choice == "2":  # Fara's 2. skill costs 10 rage gives defence
         if char.rage >= 10:
             char.deffence *= 1.5
-            char.skill2 = True
             char.rage -= 10
+            char.skill2 = True
         else:
             print("You dont have enough rage.")
             skill(char)
             return
-    if char == Character.fara and choice == "3":
+    if char == Character.fara and choice == "3":  # Fara's ult costs 50 rage and fulls hp
         if char.rage >= 50:
             char.health = 600
             char.rage -= 50
@@ -98,9 +97,9 @@ def skill(char):
             print("You dont have enough rage.")
             skill(char)
             return
-    if char == Character.ken and choice == "1":
+    if char == Character.ken and choice == "1":  # Ken's 1. skill gives 5 rage
         char.rage += 5
-    if char == Character.ken and choice == "2":
+    if char == Character.ken and choice == "2":  # Ken's 2. skill costs 10 rage, gives 50 hp and basic attacks
         if char.rage >= 10:
             char.health += 50
             char.skill2 = True
@@ -109,7 +108,7 @@ def skill(char):
             print("You dont have enough rage.")
             skill(char)
             return
-    if char == Character.ken and choice == "3":
+    if char == Character.ken and choice == "3":  # Ken's ult costs 50 rage and gives 1.5x attack and deffence
         if char.rage >= 50:
             char.deffence *= 1.5
             char.attack *= 1.5
@@ -121,26 +120,22 @@ def skill(char):
             return
 
 
-def removeUlt(char):
-    # This is removing the ult effect for 1 round ults like Kevin's and Ken's.
-    # This func is taking buffs from them the round after they used ults.
-    if char == Character.kevin:
-        char.attack /= 2
-    if char == Character.fara:
-        pass
-    if char == Character.ken:
-        char.defence /= 1.5
-        char.attack /= 1.5
-    char.ult = False
-
-
-def removeSkill(char):
-    # This is removing the skill effect for 1 round skills.
-    # This func is taking buffs from them the round after they used skills.
-    if char == Character.kevin:
-        char.attack /= 1.5
-    if char == Character.fara:
-        char.deffence /= 1,5
-    if char == Character.ken:
-        pass
-    char.skill2 = False
+def removeEffect(char):
+    # This is removing the ult or skill buffs. Health buffs are not included.
+    if char.ult:  # Removes ult effects
+        if char == Character.kevin:
+            char.attack /= 2
+        if char == Character.fara:
+            pass
+        if char == Character.ken:
+            char.deffence /= 1.5
+            char.attack /= 1.5
+        char.ult = False
+    if char.skill2:  # Removes skill effects
+        if char == Character.kevin:
+            char.attack /= 2
+        if char == Character.fara:
+            char.deffence /= 1.5
+        if char == Character.ken:
+            pass
+        char.skill2 = False
